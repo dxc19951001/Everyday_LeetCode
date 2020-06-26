e55a91f43082e15716266dca0c512b27b3cee0b1
dxc19951001 <870200615@qq.com>
2020-06-26 22:45:59 +0800

    Add Comment

--- 139.单词拆分.py
+++ 139.单词拆分.py
@@ -31,24 +31,31 @@ class Solution(object):
         # 核心思想动态规划
         # 初始化dp=[False,⋯,False]，
         # 长度为n+1。n为字符串长度。dp[i]表示s的前i位是否可以用wordDict中的单词表示。
+        # 加1是为了初始dp[0]，表示没有字符串时，令dp[0]=True
 
-        # 初始化dp[0]=Truedp[0]=True，空字符可以被表示。
-        # 遍历字符串的所有子串，遍历开始索引i，遍历区间[0,n)：
-        # 遍历结束索引j，遍历区间[i+1,n+1)：
+        # 因为s要被空格拆分为一个或多个在字典中出现的单词，
+        # 首先要满足s[:i]中有符合要求的单词
+        # 所以dp[i] = True,i节点是可以拆开字符串s的节点，s[:i]为符合要求的单词
+        # 若从i节点往后到j节点，s[i:j]也存在符合要求的单词，则dp[j] = True
+        # 因此判断dp列表中为True的节点，需要dp[i] = True 和 (s[i:j] in wordDict)
 
-        # 若dp[i]=True且s[i,⋯,j)在wordlist中：dp[j]=Truedp[j]=True。
-        # 解释：dp[i]=True说明s的前i位s[:i]存在可以用wordDict表示，
-        # 则s[i,⋯,j)出现在wordDict中，说明s[i,j]可以用wordDict表示。
-        # 最终判断dp中最后是否为True
+        # 最终判断最后一个节点dp[-1]是否为True
 
         n = len(s)
         dp = [False]*(n+1)
         dp[0] = True
         for i in range(n):
+            # 循环遍历字符串长度，找到分割点i
             for j in range(i+1, n+1):
+                # 循环遍历，找到s[i:j]中符合要求的字符串，
+
+                # i+1：表示从i节点的下一位开始找
+                # n+1：由于range左闭右开，n+1只能取到n
+                # 而字符串的切片也是左闭右开，n只能取到n+1，正好取到字符串最后一位
+
                 if(dp[i] and (s[i:j] in wordDict)):
-                    print(s[i:j])
-                    print(i, j)
+                    # 若i节点为true，可以以i节点分开，且s[i:j]在字符串中
+                    # 则j节点也为true
                     dp[j] = True
-        print(dp)
+
         return dp[-1]
